2ec6ab6f8d2179408422fc4ebef39f4e169fc489
TomQuez <tom.quez@live.fr>
2023-11-20 14:42:32 +0100

    modification affichage admin

--- project/MyCollection/CollectionCatalog/admin.py
+++ project/MyCollection/CollectionCatalog/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from CollectionCatalog.models import  Collection, Category,Book, Watch, CollectionObject
 # Register your models here.
-admin.site.register(Collection)
-admin.site.register(Category)
-admin.site.register(CollectionObject)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=('name','category')
+    list_filter=('category','name')
 
-admin.site.register(Book)
-admin.site.register(Watch)
+class CollectionObjectAdmin(admin.ModelAdmin):
+    list_display=('name','id')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display=('name','author','type','edition')
+    list_filter=('name','type','edition')
+
+class WatchAdmin(admin.ModelAdmin):
+    list_display=('name','brand','type_of_mecanism')
+    list_filter=('name','brand','type_of_mecanism')
+
+admin.site.register(Collection,CollectionAdmin)
+admin.site.register(Category)
+admin.site.register(CollectionObject,CollectionObjectAdmin)
+
+admin.site.register(Book,BookAdmin)
+admin.site.register(Watch,WatchAdmin)
 
